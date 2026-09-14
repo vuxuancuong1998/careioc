354b122e607bc0f8a95f2aa2c70c3d446448912b
HaiK22TT <nchai.k22tt@kontum.udn.vn>
2026-09-14 15:59:50 +0700

    chore: add .gitignore and untrack internal test scripts

--- test.php
+++
@@ -1,2 +0,0 @@
-z<?php
-phpinfo();
--- tests/check_roles_cols.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('__SITE_PATH', realpath(__DIR__ . '/..'));
-require_once __DIR__ . '/../config.php';
-require_once __DIR__ . '/../includes/init.php';
-global $db;
-$db->query("DESCRIBE ioc_auth_roles");
-while ($r = $db->fetch_object(true)) {
-    echo $r->Field . ' (' . $r->Type . ')' . PHP_EOL;
-}
